859100e726795ec745f032010ab21e5b0bd7e265
uki00a <uki00a@gmail.com>
2018-12-02 14:31:56 +0900

    Move theme definition from src/containers/app.js to src/theme.js

---
+++ src/theme.js
@@ -0,0 +1,31 @@
+const red = '#ff0000';
+const white = '#ffffff';
+const blue = '#0000d3';
+const green = '#00cfd0';
+const cyan = '#00ffff';
+const black = '#000000';
+const yellow = '#ffff00';
+const lightGray = '#e7e7e7';
+
+export default Object.freeze({
+  box: {
+    normal: {
+      bg: blue,
+      fg: yellow,
+      border: { fg: white, bg: blue },
+      scrollbar: { bg: green },
+      label: { fg: white, bg: blue }
+    },
+    focus: {
+      border: { fg: cyan }
+    }
+  },
+  button: {
+    fg: black,
+    bg: lightGray,
+    focus: {
+      bg: green,
+      fg: black
+    }
+  }
+});
