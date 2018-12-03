96f9f804335b44995ad9d043ee9faf5c79f1a223
uki00a <uki00a@gmail.com>
2018-12-04 03:56:56 +0900

    Add <KeyContent> component

---
+++ src/components/key-content.js
@@ -0,0 +1,8 @@
+import React from 'react';
+
+const KeyContent = ({ content, theme }) => (
+  <box content={content} style={theme.box.normal}>
+  </box>
+);
+
+export default KeyContent;
--- src/containers/database.js
+++ src/containers/database.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { scanKeys } from '../modules/keys';
 import KeyList from '../components/key-list';
+import KeyContent from '../components/key-content';
 import theme from '../theme';
 
 class Database extends Component {
@@ -12,8 +13,16 @@ class Database extends Component {
   render() {
     // FIXME
     return (
-      <KeyList keys={this.props.keys} theme={theme}>
-      </KeyList>
+      <box position={{ top: 1, left: 1, bottom: 2, right: 3 }}>
+        <box position={{ left: 0, top: 0, bottom: 0, width: 30 }}>
+          <KeyList keys={this.props.keys} theme={theme}>
+          </KeyList>
+        </box>
+        <box position={{ left: 30, top: 0, right: 0 }}>
+          <KeyContent content='test' theme={theme}>
+          </KeyContent>
+        </box>
+      </box>
     );
   }
 }
