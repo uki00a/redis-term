628922d27cb8170785c5326ab075d16251666dd0
uki00a <uki00a@gmail.com>
2019-02-02 17:07:17 +0900

    Make <HashContent> scrollable

--- src/components/hash-content.js
+++ src/components/hash-content.js
@@ -3,6 +3,7 @@ import List from './list';
 import Editor from './editor';
 import PropTypes from 'prop-types';
 import KeyValueDialog from './key-value-dialog';
+import ScrollableBox from './scrollable-box';
 
 class HashContent extends Component {
   static propTypes = {
@@ -14,7 +15,7 @@ class HashContent extends Component {
     reload: PropTypes.func.isRequired
   };
 
-  state = { selectedIndex: null };
+  state = { selectedFieldIndex: null };
 
   _addRow = (field, value) => this.props.addRow(field, value);
   _openAddKeyValueDialog = () => {
@@ -22,29 +23,38 @@ class HashContent extends Component {
   };
 
   _saveField = () => {
-    if (this.state.selectedIndex == null) {
+    if (!this._hasSelectedField()) {
       return;
     }
 
     const fields = Object.keys(this.props.hash);
-    const field = fields[this.state.selectedIndex];
+    const field = fields[this.state.selectedFieldIndex];
     const newValue = this.refs.editor.value();
 
     this.props.saveField(field, newValue);
   };
 
   _onFieldSelected = (item, fieldIndex) => {
-    this.setState({ selectedIndex: fieldIndex });
+    this.setState({ selectedFieldIndex: fieldIndex });
   };
+
+  _hasSelectedField() {
+    return this.state.selectedFieldIndex != null;
+  }
+
+  _selectedFieldValue() {
+    if (this._hasSelectedField()) {
+      const fields = Object.keys(this.props.hash);
+      const selectedField = fields[this.state.selectedFieldIndex];
+      return this.props.hash[selectedField];
+    } else {
+      return null;
+    }
+  }
 
   render() {
     const fields = Object.keys(this.props.hash);
-    const selectedField = this.state.selectedIndex == null
-      ? null
-      : fields[this.state.selectedIndex];
-    const selectedFieldValue = selectedField 
-      ? this.props.hash[selectedField]
-      : null;
+    const selectedFieldValue = this._selectedFieldValue();
 
     return (
       <form>
@@ -59,17 +69,18 @@ class HashContent extends Component {
           style={this.props.theme.list}
           onSelect={this._onFieldSelected}
         />
-        <box position={{ left: '50%', top: 1 }}>
+        <ScrollableBox
+          position={{ left: '50%', top: 1, height: '90%' }}>
           <Editor
             ref='editor'
-            position={{ height: 30 }}
+            position={{ height: 30, width: '95%' }}
             defaultValue={selectedFieldValue}
-            disabled={this.state.selectedIndex == null}
+            disabled={!this._hasSelectedField()}
           />
           <button
             clickable
             mouse
-            position={{ top: 30, height: 3 }}
+            position={{ top: 30, height: 3, width: '95%' }}
             tags
             border='line'
             onClick={this._saveField}
@@ -77,7 +88,7 @@ class HashContent extends Component {
           <button
             clickable
             mouse
-            position={{ top: 33, height: 3 }}
+            position={{ top: 33, height: 3, width: '95%' }}
             tags
             border='line'
             onClick={this._openAddKeyValueDialog}
@@ -85,12 +96,12 @@ class HashContent extends Component {
           <button
             clickable
             mouse
-            position={{ top: 36, height: 3 }}
+            position={{ top: 36, height: 3, width: '95%' }}
             tags
             border='line'
             onClick={this.props.reload}
             content='{center}Reload{/center}' />
-        </box>
+        </ScrollableBox>
         <KeyValueDialog
           ref='keyValueDialog'
           theme={this.props.theme}
