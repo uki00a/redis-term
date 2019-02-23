e0387dcdd2c3797ae2a1c5dd91fbd90b1acf7fec
uki00a <uki00a@gmail.com>
2019-02-24 02:08:05 +0900

    Add support for toggling transparency of button

--- src/components/button.js
+++ src/components/button.js
@@ -1,16 +1,23 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-const Button = ({ disabled, onClick, ...restProps }) => (
-  <button
-    keys
-    position={{ top: 9, height: 1, left: 2, width: 6 }}
-    mouse
-    clickable
-    onClick={disabled ? null : onClick}
-    {...restProps}
-  />
-);
+
+const Button = ({ disabled, onClick, style, ...restProps }) => {
+  const props = {
+    keys: true,
+    mouse: true,
+    clickable: true,
+    onClick: disabled ? null : onClick,
+    ...restProps
+  };
+
+  return (
+    <button
+      style={Object.assign({ transparent: Boolean(disabled) }, style)}
+      {...props}
+    />
+  );
+};
 
 Button.propTypes = { disabled: PropTypes.bool };
 
