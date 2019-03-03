ffe361ed6d482ddb0a41e2b2c55fff5db57adad3
uki00a <uki00a@gmail.com>
2019-03-03 18:37:53 +0900

    Add SSL form to <ConfigForm>

--- src/components/config-form.js
+++ src/components/config-form.js
@@ -16,69 +16,124 @@ class ConfigForm extends Component {
 
   render() {
     const { theme } = this.props;
+    const boxStyle = Object.assign({}, theme.box, theme.box.focus);
 
     return (
       <form
         keys
         ref='form'
         border='line'
-        style={Object.assign({}, theme.box, theme.box.focus)}
+        style={boxStyle}
         position={{ left: 1, right: 1, top: 0, bottom: 0 }}
         onSubmit={this.props.onSubmit}>
         <box
+          label='Database'
+          border='line'
           style={theme.box}
-          position={{ left: 1, right: 1, top: 1, bottom: 1, width: 36 }}>
-          <box position={{ left: 0, top: 0, height: 2 }} style={theme.box}>
-            <text
-              content='Name:'
-              style={theme.box}
-              position={{ left: 0 }}>
-            </text>
-            <Textbox
-              name='name'
-              value='FIXME'
-              position={{ left: 6, height: 1, width: 16 }}>
-            </Textbox>
+          width={36}
+          height={18}>
+          <box position={{ left: 1, right: 1, top: 1, bottom: 1 }} style={theme.box}>
+            <box position={{ left: 0, top: 0, height: 2 }} style={theme.box}>
+              <text
+                content='Name:'
+                style={theme.box}
+                position={{ left: 0 }}>
+              </text>
+              <Textbox
+                name='name'
+                value='FIXME'
+                position={{ left: 6, height: 1, width: 16 }}>
+              </Textbox>
+            </box>
+            <box position={{ left: 0, top: 2, height: 2 }} style={theme.box}>
+              <text
+                content='Host:'
+                style={theme.box}
+                position={{ left: 0 }}>
+              </text>
+              <Textbox
+                name='host'
+                value='127.0.0.1'
+                position={{ left: 6, height: 1, width: 16 }}>
+              </Textbox>
+            </box>
+            <box position={{ left: 0, top: 4, height: 2 }} style={theme.box}>
+              <text
+                content='Port:'
+                style={theme.box}
+                position={{ left: 0 }}>
+              </text>
+              <Textbox
+                name='port'
+                value='6379'
+                position={{ left: 6, height: 1, width: 16 }}>
+              </Textbox>
+            </box>
+            <box position={{ left: 0, top: 6, height: 2 }} style={theme.box}>
+              <text
+                content='Password:'
+                style={theme.box}
+                position={{ left: 0 }}>
+              </text>
+              <Textbox
+                name='password'
+                value=''
+                position={{ left: 10, height: 1, width: 16 }}>
+              </Textbox>
+            </box>
           </box>
-          <box position={{ left: 0, top: 2, height: 2 }} style={theme.box}>
-            <text
-              content='Host:'
-              style={theme.box}
-              position={{ left: 0 }}
-            ></text>
-            <Textbox
-              name='host'
-              value='127.0.0.1'
-              position={{ left: 6, height: 1, width: 16 }}>
-            </Textbox>
+        </box>
+        <box
+          label='SSL'
+          border='line'        
+          position={{ left: 36, top: 0, height: 14 }}
+          style={theme.box}>
+          <box position={{ left: 1, top: 1, height: 1, right: 1 }} style={theme.box}>
+            <box position={{ left: 0, top: 0, height: 2 }} style={theme.box}>
+              <text
+                content='Private Key:'
+                style={theme.box}
+                position={{ left: 0 }}>
+              </text>
+              <Textbox
+                name='tlskey'
+                value=''
+                position={{ left: 14, height: 1, width: 16 }}>
+              </Textbox>
+            </box>
+            <box position={{ left: 0, top: 2, height: 2 }} style={theme.box}>
+              <text
+                content='Certificate:'
+                style={theme.box}
+                position={{ left: 0 }}>
+              </text>
+              <Textbox
+                name='tlscert'
+                value=''
+                position={{ left: 14, height: 1, width: 16 }}>
+              </Textbox>
+            </box>
+            <box position={{ left: 0, top: 4, height: 2 }} style={theme.box}>
+              <text
+                content='CA:'
+                style={theme.box}
+                position={{ left: 0 }}>
+              </text>
+              <Textbox
+                name='tlscert'
+                value=''
+                position={{ left: 14, height: 1, width: 16 }}>
+              </Textbox>
+            </box>
           </box>
-          <box position={{ left: 0, top: 4, height: 2 }} style={theme.box}>
-            <text
-              content='Port:'
-              style={theme.box}
-              position={{ left: 0 }}>
-            </text>
-            <Textbox
-              name='port'
-              value='6379'
-              position={{ left: 6, height: 1, width: 16 }}>
-            </Textbox>
-          </box>
-          <box position={{ left: 0, top: 6, height: 2 }} style={theme.box}>
-            <text
-              content='Password:'
-              style={theme.box}
-              position={{ left: 0 }}>
-            </text>
-            <Textbox
-              name='password'
-              value=''
-              position={{ left: 10, height: 1, width: 16 }}>
-            </Textbox>
-          </box>
-          <box position={{ left: 0, top: 8, height: 2 }} style={theme.box}>
+        </box>
+        <box 
+          border='line'
+          position={{ left: 36, top: 14, height: 4 }}
+          style={theme.box}>
+          <box position={{ left: 1, top: 1, height: 1, right: 1 }} style={theme.box}>
             <ThemedButton
-              position={{ left: 36, height: 1, width: 16 }}
+              position={{ height: 1, width: 16 }}
               content=' Connect '
               onPress={this.onConnectButtonClicked}>
             </ThemedButton>
