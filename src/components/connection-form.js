40c03d50110b07464d8638067afe9b82a2ccc9e5
uki00a <uki00a@gmail.com>
2019-03-11 01:43:24 +0900

    Move options normalization logic from ConnectionForm to ConnectionFromContainer

--- src/components/connection-form.js
+++ src/components/connection-form.js
@@ -1,4 +1,3 @@
-import fs from 'fs';
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import Textbox from './textbox';
@@ -17,67 +16,8 @@ class ConfigForm extends Component {
   };
 
   _handleSubmit = options => {
-    const normalizedOptions = this._normalizeOptions(options);
-    this.props.onSubmit(normalizedOptions);
+    this.props.onSubmit(options);
   };
-
-  _normalizeOptions(options) {
-    return this._normalizeSSHOptions(this._normalizeTLSOptions(options));
-  }
-
-  _normalizeTLSOptions(options) {
-    if (this._containsTLSOptions(options)) {
-      // TODO cleanup
-      const { tlskey, tlscert, tlsca, ...restOptions } = options;
-      restOptions.tls = {};
-      restOptions.tls.key = this._readIfExists(tlskey);
-      restOptions.tls.cert = this._readIfExists(tlscert);
-      restOptions.tls.ca = this._readIfExists(tlsca);
-      return restOptions;
-    } else {
-      return options;
-    }
-  }
-
-  _containsTLSOptions(options) {
-    const tlsOptions = ['tlskey', 'tlscert', 'tlsca'];
-    return tlsOptions.some(option => options[option]);
-  }
-
-  _normalizeSSHOptions(options) {
-    if (this._containsSSHOptions(options)) {
-      // TODO cleanup
-      const {
-        sshhost,
-        sshport,
-        sshuser,
-        sshprivateKeyPath,
-        sshpassword,
-        ...restOptions
-      } = options;
-      restOptions.ssh = {
-        host: sshhost,
-        port: sshport,
-        username: sshuser,
-        privateKey: this._readIfExists(sshprivateKeyPath),
-        password: sshpassword
-      };
-      return restOptions;
-    } else  {
-      return options;
-    }
-  }
-
-  _containsSSHOptions(options) {
-    const sshOptions = ['sshhost', 'sshport', 'sshuser', 'sshprivateKeyPath', 'sshpassword'];
-    return sshOptions.some(option => options[option]);
-  }
-
-  _readIfExists(path) {
-    if (fs.existsSync(path)) {
-      return fs.readFileSync(path);
-    }
-  }
 
   _openFileManager = ref => {
     // TODO rename
--- src/containers/connection-form.js
+++ src/containers/connection-form.js
@@ -1,3 +1,4 @@
+import * as fs from 'fs';
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import ConnectionForm from '../components/connection-form';
@@ -7,9 +8,68 @@ class ConnectionFormContainer extends Component {
     connectToRedis: PropTypes.func.isRequired
   };
 
-  onSubmit = config => {
-    this.props.connectToRedis(config);
+  onSubmit = options => {
+    const normalizedOptions = this._normalizeOptions(options);
+    this.props.connectToRedis(normalizedOptions);
   };
+
+  _normalizeOptions(options) {
+    return this._normalizeSSHOptions(this._normalizeTLSOptions(options));
+  }
+
+  _normalizeTLSOptions(options) {
+    if (this._containsTLSOptions(options)) {
+      // TODO cleanup
+      const { tlskey, tlscert, tlsca, ...restOptions } = options;
+      restOptions.tls = {};
+      restOptions.tls.key = this._readIfExists(tlskey);
+      restOptions.tls.cert = this._readIfExists(tlscert);
+      restOptions.tls.ca = this._readIfExists(tlsca);
+      return restOptions;
+    } else {
+      return options;
+    }
+  }
+
+  _containsTLSOptions(options) {
+    const tlsOptions = ['tlskey', 'tlscert', 'tlsca'];
+    return tlsOptions.some(option => options[option]);
+  }
+
+  _normalizeSSHOptions(options) {
+    if (this._containsSSHOptions(options)) {
+      // TODO cleanup
+      const {
+        sshhost,
+        sshport,
+        sshuser,
+        sshprivateKeyPath,
+        sshpassword,
+        ...restOptions
+      } = options;
+      restOptions.ssh = {
+        host: sshhost,
+        port: sshport,
+        username: sshuser,
+        privateKey: this._readIfExists(sshprivateKeyPath),
+        password: sshpassword
+      };
+      return restOptions;
+    } else  {
+      return options;
+    }
+  }
+
+  _containsSSHOptions(options) {
+    const sshOptions = ['sshhost', 'sshport', 'sshuser', 'sshprivateKeyPath', 'sshpassword'];
+    return sshOptions.some(option => options[option]);
+  }
+
+  _readIfExists(path) {
+    if (fs.existsSync(path)) {
+      return fs.readFileSync(path);
+    }
+  }
 
   render() {
     return <ConnectionForm onSubmit={this.onSubmit}></ConnectionForm>;
