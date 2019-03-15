1b9fc0c82298bc405fc58549a1cdd8da5c50bfb5
uki00a <uki00a@gmail.com>
2019-03-15 23:09:14 +0900

    Fix indentation

--- src/modules/redux/keyboard-bindings/index.test.js
+++ src/modules/redux/keyboard-bindings/index.test.js
@@ -3,32 +3,32 @@ import reducer, { actions } from './index';
 import { noop } from '../../utils';
 
 describe('keyboard bindings duck', () => {
-   it('can handle ENABLE_KEYBOARD_BINDINGS', () => {
-     const previousKeyboardBindings = [
-       { key: 'C-c', handler: noop, description: 'test' }
-     ];
-     const givenKeyboardBindings = [
+    it('can handle ENABLE_KEYBOARD_BINDINGS', () => {
+      const previousKeyboardBindings = [
+        { key: 'C-c', handler: noop, description: 'test' }
+      ];
+      const givenKeyboardBindings = [
+        { key: 'C-r', handler: noop, description: 'Reload' },
+        { key: 'C-d', handler: noop, description: 'Delete Key' }
+      ];
+      const action = actions.enableKeyboardBindings(givenKeyboardBindings);
+      const result = reducer(previousKeyboardBindings, action);
+
+      assert.deepEqual(result, previousKeyboardBindings.concat(givenKeyboardBindings));
+    }); 
+
+    it('can handle DISABLE_KEYBOARD_BINDINGS', () => {
+      const previousKeyboardBindings = [
+       { key: 'C-c', handler: noop, description: 'test' },
        { key: 'C-r', handler: noop, description: 'Reload' },
        { key: 'C-d', handler: noop, description: 'Delete Key' }
-     ];
-     const action = actions.enableKeyboardBindings(givenKeyboardBindings);
-     const result = reducer(previousKeyboardBindings, action);
+      ];
+      const action = actions.disableKeyboardBindings([
+        { key: 'C-r', hanler: noop, description: 'Reload' },
+        { key: 'C-d', handler: noop, description: 'Delete Key' }
+      ]);
+      const result = reducer(previousKeyboardBindings, action);
 
-     assert.deepEqual(result, previousKeyboardBindings.concat(givenKeyboardBindings));
-   }); 
-
-   it('can handle DISABLE_KEYBOARD_BINDINGS', () => {
-     const previousKeyboardBindings = [
-      { key: 'C-c', handler: noop, description: 'test' },
-      { key: 'C-r', handler: noop, description: 'Reload' },
-      { key: 'C-d', handler: noop, description: 'Delete Key' }
-     ];
-     const action = actions.disableKeyboardBindings([
-       { key: 'C-r', hanler: noop, description: 'Reload' },
-       { key: 'C-d', handler: noop, description: 'Delete Key' }
-     ]);
-     const result = reducer(previousKeyboardBindings, action);
-
-     assert.deepEqual(result, [{ key: 'C-c', handler: noop, description: 'test' }])
-   });
+      assert.deepEqual(result, [{ key: 'C-c', handler: noop, description: 'test' }])
+    });
 });
