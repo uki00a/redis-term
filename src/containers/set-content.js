dc46184d1cc8a38d9b611311c4cb9669fa195ef3
uki00a <uki00a@gmail.com>
2019-03-22 03:44:38 +0900

    Simplify set content view

--- src/containers/set-content.js
+++ src/containers/set-content.js
@@ -2,9 +2,17 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
-import SetContent from '../components/set-content';
+import List from '../components/list';
+import FilterableList from '../components/filterable-list';
+import Editor from '../components/editor';
+import Prompt from '../components/prompt';
+import ThemedButton from '../components/themed-button';
+import ScrollableBox from '../components/scrollable-box';
+import ConfirmationDialog from '../components/confirmation-dialog';
 import Loader from '../components/loader';
+import KeyboardBindings from './keyboard-bindings';
 import { operations } from '../modules/redux/set';
+import { withTheme } from '../contexts/theme-context';
 
 class SetContentContainer extends Component {
   static propTypes = {
@@ -17,6 +25,20 @@ class SetContentContainer extends Component {
     filterSetMembers: PropTypes.func.isRequired
   };
 
+  state = { editingMemberIndex: null };
+
+  _openAddMemberPrompt = () => {
+    this.refs.addMemberPrompt.open();
+  };
+
+  _closeAddMemberPrompt = () => {
+    this.refs.addMemberPrompt.close();
+  };
+
+  _openConfirmationDialog = () => {
+    this.refs.confirmationDialog.open();
+  };
+
   _addMember = async newMember => {
     if (!newMember) {
       return;
@@ -26,7 +48,53 @@ class SetContentContainer extends Component {
 
   _reload = () => {
     this.props.filterSetMembers(this.props.pattern);
+    this._unselectMember();
   };
+
+  _saveEditingMember = () => {
+    if (!this._hasEditingMember()) {
+      return;
+    }
+
+    const oldValue = this.props.members[this.state.editingMemberIndex];
+    const newValue = this.refs.editor.value();
+    this.props.updateSetMember(oldValue, newValue);
+  };
+
+  _removeHoveredMember = () => {
+    const index = this._hoveredMemberIndex();    
+    const memberToRemove = this.props.members[index];
+    if (memberToRemove) {
+      this._removeMember(memberToRemove);
+    }
+  };
+
+  _removeMember(member) {
+    this.props.deleteMemberFromSet(member);
+    this._unselectMember();
+  }
+
+  _hasEditingMember() {
+    return this.state.editingMemberIndex != null;
+  }
+
+  _editingValue() {
+    return this._hasEditingMember()
+      ? this.props.members[this.state.editingMemberIndex]
+      : '';
+  }
+
+  _onMemberSelected = (item, index) => {
+    this.setState({ editingMemberIndex: index });
+  };
+
+  _unselectMember() {
+    this.setState({ editingMemberIndex: null });
+  }
+
+  _hoveredMemberIndex() {
+    return this.refs.memberList.selected();
+  }
 
   async componentDidMount() {
     this.props.filterSetMembers();
@@ -35,20 +103,68 @@ class SetContentContainer extends Component {
   render() {
     if (this.props.isLoading) {
       return <Loader />;
-    } else {
-      return (
-        <SetContent
-          keyName={this.props.keyName}
-          members={this.props.members}
-          addMember={this._addMember}
-          reload={this._reload}
-          filterMembers={this.props.filterSetMembers}
-          saveMember={this.props.updateSetMember}
-          removeMember={this.props.deleteMemberFromSet}
-          lastPattern={this.props.pattern}
+    } 
+
+    const hasSelectedMember = this._hasEditingMember();
+    const memberList = (
+      <KeyboardBindings bindings={[
+        { key: 'C-r', handler: this._reload, description: 'Reload' },
+        { key: 'a', handler: this._openAddMemberPrompt, description: 'Add Member' },
+        { key: 'd', handler: this._openConfirmationDialog, description: 'Delete Member' }
+      ]}>
+        <List
+          ref='memberList'
+          items={this.props.members}
+          onSelect={this._onMemberSelected}
         />
-      );
-    }
+      </KeyboardBindings>
+    );
+
+    return (
+      <box style={this.props.theme.box}>
+        <box
+          style={this.props.theme.box}
+          content={this.props.keyName}
+          position={{ height: 1 }}
+          bold
+        />
+        <FilterableList
+          position={{ width: '50%', top: 1 }}
+          List={memberList}
+          filterList={this.props.filterSetMembers}
+          defaultPattern={this.props.lastPattern}
+        />
+        <ScrollableBox
+          style={this.props.theme.box}
+          position={{ left: '50%', top: 1, height: '90%' }}>
+          <Editor
+            ref='editor'
+            position={{ height: 25, width: '95%' }}
+            defaultValue={this._editingValue()}
+            disabled={!hasSelectedMember}
+          />
+          <ThemedButton
+            disabled={!hasSelectedMember}
+            content='{center}Save{/center}'
+            tags
+            position={{ top: 25, left: 1, height: 1, width: 8 }}
+            onClick={this._saveEditingMember}
+          />
+        </ScrollableBox>
+        <Prompt
+          position={{ height: 20 }}
+          ref='addMemberPrompt'
+          title='Add Member'
+          onOk={this._addMember}
+          onCancel={this._closeAddMemberPrompt}
+        />
+        <ConfirmationDialog
+          text='Are you sure you want to delete this member'
+          onOk={this._removeHoveredMember}
+          ref='confirmationDialog'
+        />
+      </box>
+    );
   }
 }
 
@@ -63,5 +179,5 @@ const mapDispatchToProps = {
 export default connect(
   mapStateToProps,
   mapDispatchToProps
-)(SetContentContainer);
+)(withTheme(SetContentContainer));
 
