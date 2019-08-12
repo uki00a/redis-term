9c122efe07e42d4144e7934e5daee321466b3593
uki00a <uki00a@gmail.com>
2019-08-12 22:30:33 +0900

    test: add test for adding a new field to hash

--- integration-tests/containers/hash-content.test.js
+++ integration-tests/containers/hash-content.test.js
@@ -39,28 +39,71 @@ describe('<HashContentContainer>', () => {
 
       const { queryBy, getByType } = await renderSubject({ keyName });
       const textarea = getByType('textarea');
-      const list = getByType('list');
+      const fieldList = getByType('list');
 
-      assert.strictEqual(list.ritems.length, 2);
-      assert(fields.every(field => list.ritems.includes(field)));
+      assert.strictEqual(fieldList.ritems.length, 2);
+      assert(fields.every(field => fieldList.ritems.includes(field)));
 
-      list.focus();
+      fieldList.focus();
       await nextTick();
 
-      list.select(1); 
-      list.emit('keypress', null, { name: 'enter', full: 'enter' });
+      fieldList.select(1); 
+      fieldList.emit('keypress', null, { name: 'enter', full: 'enter' });
       textarea.focus();
       await nextTick();
 
-      assert.strictEqual(textarea.getValue(), initialHash[list.ritems[1]]);
+      assert.strictEqual(textarea.getValue(), initialHash[fieldList.ritems[1]]);
       const newValueOfSecondField = faker.address.city();
       textarea.setValue(newValueOfSecondField);
       textarea.emit('keypress', null, { full: 'C-s' });
       await waitFor(() => queryBy(x => x.name === 'loader') == null);
 
-      const expected = { ...initialHash, [list.ritems[1]]: newValueOfSecondField };
-      assert(fields.every(field => list.ritems.includes(field)))
+      const expected = { ...initialHash, [fieldList.ritems[1]]: newValueOfSecondField };
+      assert(fields.every(field => fieldList.ritems.includes(field)))
       assert.deepEqual(await redis.getHashFields(keyName), expected);
+    });
+  });
+
+  describe('adding a new field to hash', () => {
+    beforeEach(async () => {
+      redis = await connectToRedis();
+      screen = createScreen();
+    });
+
+    afterEach(async () => {
+      await cleanupRedisConnection(redis);
+      screen.destroy();
+    });
+
+    it('can add a new field to hash', async () => {
+      const keyName = faker.random.word();
+      const initialHash = { [faker.random.word()]: faker.random.word() };
+      const initialFields = Object.keys(initialHash);
+      await saveHashToRedis(keyName, initialHash);
+
+      const { queryBy, getBy, getByType, getByContent } = await renderSubject({ keyName });
+      const fieldList = getByType('list');
+
+      assert.strictEqual(fieldList.ritems.length, 1);
+      assert(initialFields.every(field => fieldList.ritems.includes(field)));
+
+      fieldList.focus();
+      fieldList.emit('keypress', null, { name: 'a', full: 'a' });
+
+      const keyInput = getBy(x => x.name === 'keyInput');
+      const valueInput = getBy(x => x.name === 'valueInput');
+      const okButton = getByContent(/OK/i);
+      const newKey = faker.name.title();
+      const newValue = faker.name.jobTitle();
+
+      keyInput.setValue(newKey);
+      valueInput.setValue(newValue);
+      okButton.emit('click');
+      await waitFor(() => queryBy(x => x.name === 'loader') === null);
+
+      const expected = { ...initialHash, [newKey]: newValue };
+      assert.deepEqual(await redis.getHashFields(keyName), expected);
+      assert(Object.keys(expected).every(field => fieldList.ritems.includes(field)));
     });
   });
 
