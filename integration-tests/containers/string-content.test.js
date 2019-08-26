df91b82609aea3dbf64344b85475ae57c46772fe
uki00a <uki00a@gmail.com>
2019-08-26 22:44:29 +0900

    test: shallow nesting

--- integration-tests/containers/string-content.test.js
+++ integration-tests/containers/string-content.test.js
@@ -13,58 +13,58 @@ import assert from 'assert';
 import fixtures from '../fixtures';
 
 describe('<StringContentContainer>', () => {
-  context('when C-s pressed', () => {
-    /** @type {import('../../src/modules/redis/facade').default} */
-    let redis;
-    let screen;
+  /** @type {import('../../src/modules/redis/facade').default} */
+  let redis;
+  let screen;
 
-    before(async () => {
-      redis = await connectToRedis();
-      screen = createScreen();
-    });
-
-    after(async () => {
-      await cleanupRedisConnection(redis);
-      screen.destroy();
-    });
-
-    it('should save input value to redis', async () => {
-      const keyName = fixtures.redisKey();
-      const initialValue = fixtures.string();
-      await saveString(keyName, initialValue);
-
-      const {getByType, queryBy} = await renderSubject({ keyName });
-      const textarea = getByType('textarea');
-
-      assert.strictEqual(textarea.getValue(), initialValue);
-
-      textarea.focus();
-      await nextTick();
-
-      const newValue = fixtures.string();
-      textarea.setValue(newValue);
-      textarea.emit('keypress', null, { full: 'C-s' });
-      await waitFor(() => queryBy(x => x.name === 'loader') == null);
-
-      const expected = newValue;
-      const actual = await redis.loadString(keyName);
-      assert.strictEqual(actual, expected);
-      assert.strictEqual(textarea.getValue(), expected);
-    });
-
-    const renderSubject = async ({ keyName }) => {
-      const store = createStore({
-        state: { keys: { selectedKeyName: keyName, selectedKeyType: 'string' } },
-        extraArgument: { redis }
-      });
-      const subject = render(
-        <StringContentContainer keyName={keyName} />,
-        screen,
-        { store }
-      );
-      await waitFor(() => subject.getByType('textarea'));
-      return subject;
-    };
-    const saveString = (key, value) => redis.saveString(key, value);
+  afterEach(async () => {
+    await cleanupRedisConnection(redis);
+    screen.destroy();
   });
+
+  it('should save input value to redis when "C-s" pressed on textarea', async () => {
+    redis = await connectToRedis();
+    screen = createScreen();
+
+    const keyName = fixtures.redisKey();
+    const initialValue = fixtures.string();
+    await saveString(keyName, initialValue);
+
+    const {getByType, queryBy} = await renderSubject({
+      keyName,
+      redis,
+      screen
+    });
+    const textarea = getByType('textarea');
+
+    assert.strictEqual(textarea.getValue(), initialValue);
+
+    textarea.focus();
+    await nextTick();
+
+    const newValue = fixtures.string();
+    textarea.setValue(newValue);
+    textarea.emit('keypress', null, { full: 'C-s' });
+    await waitFor(() => queryBy(x => x.name === 'loader') == null);
+
+    const expected = newValue;
+    const actual = await redis.loadString(keyName);
+    assert.strictEqual(actual, expected);
+    assert.strictEqual(textarea.getValue(), expected);
+  });
+
+  const renderSubject = async ({ redis, screen, keyName }) => {
+    const store = createStore({
+      state: { keys: { selectedKeyName: keyName, selectedKeyType: 'string' } },
+      extraArgument: { redis }
+    });
+    const subject = render(
+      <StringContentContainer keyName={keyName} />,
+      screen,
+      { store }
+    );
+    await waitFor(() => subject.getByType('textarea'));
+    return subject;
+  };
+  const saveString = (key, value) => redis.saveString(key, value);
 });
