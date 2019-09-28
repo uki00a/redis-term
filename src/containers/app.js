264d41c8978e723af054e212873a1f2e06381612
uki00a <uki00a@gmail.com>
2019-09-29 01:45:14 +0900

    refactor: use function insted of class

--- src/containers/app.js
+++ src/containers/app.js
@@ -1,28 +1,26 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { MemoryRouter } from 'react-router';
 import PropTypes from 'prop-types';
 import RedisTerm from './redis-term';
 import { ThemeProvider } from '../contexts/theme-context';
 import { ConnectionsContainer, KeyboardBindingsContainer } from '../hooks/container';
 
-class App extends Component {
-  static propTypes = {
-    settings: PropTypes.object.isRequired
-  };
-
-  render() {
-    return (
-      <KeyboardBindingsContainer.Provider>
-        <ConnectionsContainer.Provider>
-          <ThemeProvider value={this.props.settings.colortheme}>
-            <MemoryRouter initialEntries={['/connections']}>
-              <RedisTerm />
-            </MemoryRouter>
-          </ThemeProvider>
-        </ConnectionsContainer.Provider>
-      </KeyboardBindingsContainer.Provider>
-    );
-  }
+function App({ settings }) {
+  return (
+    <KeyboardBindingsContainer.Provider>
+      <ConnectionsContainer.Provider>
+        <ThemeProvider value={settings.colortheme}>
+          <MemoryRouter initialEntries={['/connections']}>
+            <RedisTerm />
+          </MemoryRouter>
+        </ThemeProvider>
+      </ConnectionsContainer.Provider>
+    </KeyboardBindingsContainer.Provider>
+  );
 }
+
+App.propTypes = {
+  settings: PropTypes.object.isRequired
+};
 
 export default App;
