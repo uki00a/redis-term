918c72d75ca2cedd2c3edf519b28d06a7d08337b
uki00a <uki00a@gmail.com>
2019-09-29 22:03:13 +0900

    refactor: avoid class component

--- src/components/filterable-list.js
+++ src/components/filterable-list.js
@@ -1,39 +1,47 @@
-import React, { Component } from 'react';
+import React, { useRef } from 'react';
 import PropTypes from 'prop-types';
 import Textbox from './textbox';
 import { withTheme } from '../contexts/theme-context';
 
-class FilterableList extends Component {
-  static propTypes = {
-    theme: PropTypes.object.isRequired,
-    List: PropTypes.node.isRequired,
-    filterList: PropTypes.func.isRequired,
-    defaultPattern: PropTypes.string
+/**
+ * @this {never}
+ */
+function FilterableList({
+  List,
+  defaultPattern,
+  theme,
+  filterList,
+  ...restProps
+}) { 
+  const patternInput = useRef(null);
+
+  const handleSubmit = () => {
+    const pattern = patternInput.current.value();
+    filterList(pattern);
   };
 
-  _filterList = () => {
-    const pattern = this.refs.patternInput.value();
-    this.props.filterList(pattern);
-  };
-
-  render () {
-    const { List, defaultPattern, theme, ...restProps } = this.props;
-    return (
-      <box {...restProps}>
-        <box position={{ bottom: 3 }}>
-          { List }
-        </box>
-        <Textbox
-          style={theme.patternInput}
-          ref='patternInput'
-          label='Search'
-          onSubmit={this._filterList}
-          border='line'
-          position={{bottom: 0, height: 3, width: '100%'}}
-          defaultValue={defaultPattern} />
-      </box>
-    );
-  }
+  return (
+   <box {...restProps}>
+     <box position={{ bottom: 3 }}>
+       { List }
+     </box>
+     <Textbox
+       style={theme.patternInput}
+       ref={patternInput}
+       label='Search'
+       onSubmit={handleSubmit}
+       border='line'
+       position={{bottom: 0, height: 3, width: '100%'}}
+       defaultValue={defaultPattern} />
+   </box>
+  ); 
 }
+
+FilterableList.propTypes = {
+  theme: PropTypes.object.isRequired,
+  List: PropTypes.node.isRequired,
+  filterList: PropTypes.func.isRequired,
+  defaultPattern: PropTypes.string
+};
 
 export default withTheme(FilterableList);
