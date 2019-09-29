82526d8654693d6097a9daf57da251e03da19f85
uki00a <uki00a@gmail.com>
2019-09-29 22:42:54 +0900

    refactor: avoid class component

--- src/components/message-dialog.js
+++ src/components/message-dialog.js
@@ -1,64 +1,76 @@
-import React, { Component } from 'react';
+import React, { useState, useImperativeHandle, useRef, forwardRef } from 'react';
 import PropTypes from 'prop-types';
 import Dialog from './dialog';
 import ThemedButton from './themed-button';
 import { withTheme } from '../contexts/theme-context';
 
-class MessageDialog extends Component {
-  static propTypes = {
-    text: PropTypes.string,
-    theme: PropTypes.object.isRequired,
-    onHide: PropTypes.func
+/**
+ * @this {never}
+ */
+const MessageDialog = forwardRef(({
+  text,
+  position = {},
+  theme,
+  onHide,
+  ...restProps
+}, ref) => {
+  const [isOpened, setOpened] = useState(false);
+  const okButton = useRef(null);
+  const dialogPosition = { height: 'shrink', ...position };
+
+  const open = () => {
+    setOpened(true);
+    setImmediate(() => okButton.current.focus());
   };
 
-  state = { isOpened: false };
-
-  open() {
-    this.setState({ isOpened: true }, () => this.refs.okButton.focus());
-  }
-
-  close = () => {
-    this.setState({ isOpened: false });
-    this._handleMessageDialogHidden();
+  const close = () => {
+    setOpened(false);
+    _handleMessageDialogHidden();
   };
 
-  _onHide = () => {
-    this.setState({ isOpened: false });
-    this._handleMessageDialogHidden();
+  const _onHide = () => {
+    setOpened(false);
+    _handleMessageDialogHidden();
   };
 
-  _handleMessageDialogHidden() {
-    if (this.props.onHide) {
-      this.props.onHide();
+  const _handleMessageDialogHidden = () => {
+    if (onHide) {
+      onHide();
     }
-  }
+  };
 
-  render() {
-    const { text, position = {}, theme, onHide, ...restProps } = this.props;
-    const dialogPosition = { height: 'shrink', ...position };
+  useImperativeHandle(ref, () => ({
+    open,
+    close
+  }));
 
-    return (
-      <Dialog
-        isOpened={this.state.isOpened && Boolean(text)}
-        position={dialogPosition}
-        onHide={this._onHide}
-        {...restProps}>
-        <box
-          style={theme.box}
-          position={{ top: 2, left: 2, right: 2 }}
-          content={text}
-          tags
-        />
-        <ThemedButton
-          ref='okButton'
-          position={{ bottom: 1, height: 1, right: 2, width: 4 }}
-          content='OK'
-          align='center'
-          onClick={this.close}
-        />
-      </Dialog>
-    );
-  }
-}
+  return (
+    <Dialog
+      isOpened={isOpened && Boolean(text)}
+      position={dialogPosition}
+      onHide={_onHide}
+      {...restProps}>
+      <box
+        style={theme.box}
+        position={{ top: 2, left: 2, right: 2 }}
+        content={text}
+        tags
+      />
+      <ThemedButton
+        ref={okButton}
+        position={{ bottom: 1, height: 1, right: 2, width: 4 }}
+        content='OK'
+        align='center'
+        onClick={close}
+      />
+    </Dialog>
+  );
+});
+
+MessageDialog.propTypes = {
+  text: PropTypes.string,
+  theme: PropTypes.object.isRequired,
+  onHide: PropTypes.func
+};
 
 export default withTheme(MessageDialog);
