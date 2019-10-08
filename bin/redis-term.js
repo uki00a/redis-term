38c03576e8d4e2a0c468540aa486a3fdb5110b1c
uki00a <uki00a@gmail.com>
2019-10-09 03:53:06 +0900

    feat: '--version' flag

--- bin/redis-term.js
+++ bin/redis-term.js
@@ -1,3 +1,10 @@
 #!/usr/bin/env node
 
+const argv = process.argv.slice(2);
+if (argv.includes('--version')) {
+  const pkg = require('../package.json');
+  console.log(pkg.version);
+  process.exit(0);
+}
+
 require('../lib/index');
